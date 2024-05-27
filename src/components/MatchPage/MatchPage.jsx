a1ebacda42432431156a698ab0f52b541b1064fa
Matt Gilbertson <mgilbertsonund@gmail.com>
2024-05-27 10:18:53 -0500

    using params, matchpage has accessed the appropriate game when clicked on from the landingpage. Now to add details

--- src/components/MatchPage/MatchPage.jsx
+++ src/components/MatchPage/MatchPage.jsx
@@ -4,34 +4,18 @@ import { useParams } from 'react-router-dom';
 
 const MatchPage = () => {
   const { matchId } = useParams();
-  const games = useSelector(state => state.odds[matchId]);
+  const { odds } = useSelector(store => store.odds);
 
-  const game = games.find(game => game.away_team === matchId || game.home_team === matchId);
+  const selectedGame = odds.find(game => game.id === matchId);
+  console.log('selectedGame', selectedGame);
 
   return (
     <div>
-      {game && (
-        <div>
-          <h2>{game.away_team} vs. {game.home_team}</h2>
-          <p>Market: Moneyline</p>
-          {game.bookmakers.map(bookmaker => (
-            <div key={bookmaker.key}>
-              <h3>{bookmaker.title}</h3>
-              <ul>
-                {bookmaker.markets.map(market => (
-                  <li key={market.key}>
-                    <p>Market: {market.key}</p>
-                    {/* Render other market details */}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
+      {/* Display selected game details here */}
+      {selectedGame.away_team}
+      {selectedGame.home_team}
     </div>
   );
 };
 
 export default MatchPage;
-
