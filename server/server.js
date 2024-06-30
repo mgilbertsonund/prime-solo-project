9d95f183bc760108e4e916b8b6010708fa211977
Matt Gilbertson <mgilbertsonund@gmail.com>
2024-06-30 18:18:44 -0500

    fixing config

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7540;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
